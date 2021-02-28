f5d4cd062a007da6f50c76c824e08c747aea1fa5
Denis Gaetani <denisgaetani.SONDA@br-petrobras.com.br>
2021-02-28 17:40:53 -0300

    Aula 130 - projeto Burger

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import Burger from '../../components/Burger/Burger'
+import Aux from 'react-aux';
+
+class BurgerBuilder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ingredients: {
+                salad: 0,
+                bacon: 0,
+                cheese: 0,
+                meat: 0
+            }
+        }
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Burger ingredients={this.state.ingredients}/>
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
